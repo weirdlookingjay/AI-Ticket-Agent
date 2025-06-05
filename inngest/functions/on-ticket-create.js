d167c27060d3b5cb04f81dd6ab4151d7b0c8c177
Joe Clark <weirdlookingjay@live.com>
2025-06-05 15:04:25 -0400

    created a ticket create function

---
+++ inngest/functions/on-ticket-create.js
@@ -0,0 +1,86 @@
+import { inngest } from "../client";
+import Ticket from "../../models/ticket.js";
+import User from "../../models/user.js";
+import sendMail from "../../utils/mailer.js";
+import { NonRetriableError } from "inngest";
+
+import analyzeTicket from "../../utils/ai.js";
+
+export const onTicketCreated = inngest.createFunction(
+  { id: "on-ticket-created", retries: 2 },
+  { event: "ticket/created" },
+  async ({ event, step }) => {
+    try {
+      const { ticketId } = event.data;
+
+      //fetch ticket from database
+      const ticket = await step.run("fetch-ticket", async () => {
+        const ticketObject = await Ticket.findById(ticketId);
+        if (!ticketObject) {
+          throw new NonRetriableError(`Ticket with ID ${ticketId} not found`);
+        }
+        return ticketObject;
+      });
+
+      await step.run("update-ticket-status", async () => {
+        await Ticket.findByIdAndUpdate(ticket._id, { status: "TODO" });
+      });
+
+      const aiResponse = await analyzeTicket(ticket);
+
+      const relatedSkills = await step.run("ai-processing", async () => {
+        let skills = [];
+        if (aiResponse) {
+          await Ticket.findByIdAndUpdate(ticket._id, {
+            priority: !["low", "medium", "high"].includes(aiResponse.priority)
+              ? "medium"
+              : aiResponse.priority,
+            helpfulNotes: aiResponse.helpfulNotes,
+            status: "IN_PROGRESS",
+            relatedSkills: aiResponse.relatedSkills,
+          });
+          skills = aiResponse.relatedSkills;
+        }
+
+        return skills;
+      });
+
+      const moderator = await step.run("assign-moderator", async () => {
+        let user = await User.findOne({
+          role: "moderator",
+          skills: {
+            $elementMatch: {
+              $regex: relatedSkills.join("|"),
+              $options: "i",
+            },
+          },
+        });
+
+        if (!user) {
+          user = await User.findOne({ role: "admin" });
+        }
+        await Ticket.findByIdAndUpdate(ticket._id, {
+          assignedTo: user?._id || null,
+        });
+
+        return user;
+      });
+
+      await step.run("send-email-notication", async () => {
+        if (moderator) {
+          const finalTicket = await Ticket.findById(ticket._id);
+          await sendMail(
+            moderator.email,
+            "Ticket Assigned",
+            `A new ticket is assigned to yo ${finalTicket.title}`
+          );
+        }
+      });
+
+      return { success: true };
+    } catch (error) {
+      console.error("❌ Error processing ticket creation:", error.message);
+      return { success: false };
+    }
+  }
+);
