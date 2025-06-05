a144ad673b5238a9b80ba967a553173ddf46f2a9
Joe Clark <weirdlookingjay@live.com>
2025-06-05 12:40:32 -0400

    created inngest pipelines

---
+++ inngest/functions/on-signup.js
@@ -0,0 +1,41 @@
+import { inngest } from "../client";
+import User from "../../models/user.js"
+import sendMail from "../../utils/mailer.js";
+import { NonRetriableError } from "inngest";
+
+
+export const onUserSignup = inngest.createFunction(
+    {id: "on-user-signup", retries: 2},
+    {event: "user/signup"},
+    // First pipeline step: get user by email
+    async ({event, step}) => {
+        try {
+            const {email} = event.data
+            const user = await step.run("get-user-email", async () => {
+                // Simulate a database call to find the user by email
+                const userObject =await User.findOne({email})
+                if (!userObject) {
+                    throw new NonRetriableError(
+                        `User with email ${email} not found`
+                    )
+                }
+                return userObject
+            })
+            
+            // Second pipeline step: send welcome email
+           await step.run("send-welcome-email", async () => {
+                // Simulate sending a welcome email
+                const subject = `Welcome to Our Service!`;
+                const message = `Hello, ${email}! Thank you for signing up.`;
+                await sendMail(user.email, subject, message);
+                console.log(`Sending welcome email to ${email}`);
+                
+            });
+
+            return {success:true}
+        } catch (error) {
+            console.error("❌ Error in onUserSignup function:", error.message);
+            return {success: false}
+        }
+    }    
+)
---
+++ utils/mailer.js
@@ -0,0 +1,31 @@
+import  nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const sendMail = async (to, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAILTRAP_USER,
+            port: process.env.MAILTRAP_SMTP_PORT,
+            secure: false,
+            auth: {
+              user: process.env.MAILTRAP_USER,
+              pass: process.env.MAILTRAP_PASS,
+            },
+          });
+
+          const info = await transporter.sendMail({
+            from: 'Inngest Team',
+            to,
+            subject,
+            text,
+          });
+
+          console.log("Message sent:", info.messageId);
+          return info;
+    } catch (error) {
+        console.error("❌ Error sending email:", error);
+        throw new Error("Failed to send email");
+    }
+}
