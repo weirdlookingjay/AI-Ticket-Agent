fef51da2234ea4a38add82da3bd65eda65e64e9f
Joe Clark <weirdlookingjay@live.com>
2025-06-05 15:43:07 -0400

    added inngest middleware

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import { serve } from "inngest/express";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
 import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
 dotenv.config();
 
@@ -15,6 +19,11 @@ app.unsubscribe("/api/tickets", ticketRoutes);
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  "/api/inngest",
+  serve({ client: inngest, functions: [onUserSignup, onTicketCreated] })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
